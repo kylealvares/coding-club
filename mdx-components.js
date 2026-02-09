a27a4225b253eaf7fc155454f26181dab1f70390
Kyle Alvares <kyle.alv2000@gmail.com>
2026-02-08 20:08:21 -0500

    🛠️ mdx components & week five content

--- mdx-components.js
+++ mdx-components.js
@@ -1,5 +1,14 @@
 import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs' // nextra-theme-blog or your custom theme
- 
+import { Callout, Cards, Steps, Tabs } from 'nextra/components'
+import { CodeRunner } from './app/components/code-runner'
+import { ContactCards, ContactCard, RoleCard } from './app/components/contact-card'
+import { GoogleSlides } from './app/components/google-slides'
+import { Tiles, Tile } from './app/components/tiles'
+
+
+const Card = Cards.Card
+const Tab = Tabs.Tab
+
 // Get the default MDX components
 const themeComponents = getThemeComponents()
  
@@ -7,6 +16,19 @@ const themeComponents = getThemeComponents()
 export function useMDXComponents(components) {
   return {
     ...themeComponents,
-    ...components
+    ...components,
+    Callout, 
+    Cards,
+    Card,
+    CodeRunner,
+    ContactCards,
+    ContactCard,
+    GoogleSlides,
+    RoleCard,
+    Steps,
+    Tabs,
+    Tab,
+    Tiles,
+    Tile
   }
 }
