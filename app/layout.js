7c59ad25a9a1f7a8bddf443144bbbfcd88d88375
Kyle Alvares <kyle.alv2000@gmail.com>
2026-02-17 19:41:38 -0500

    🧹 cleanup

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,7 @@ export default async function RootLayout({ children }) {
       </Head>
       <body>
         <Layout
-          banner={banner}
+          // banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://kylealvares.github.io/coding-club/docs"
